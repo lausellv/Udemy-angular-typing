071710ea852a9949dc68d1eab069677eeeeb7def
Victor <lausellv@yahoo.com>
2021-10-09 18:46:28 +0200

    empty commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+  // solved = false;
+
+  onInput(value: string) {
+    this.enteredText = value;
+    // if (value === this.randomText) {
+    //   this.solved = true;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
